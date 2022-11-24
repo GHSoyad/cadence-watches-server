2cf55501723fe8a5b3da6a70c55b34b1872bcb37
GHSoyad <ghsoyad@gmail.com>
2022-11-24 23:45:05 +0600

    Added mongodb, users collection and categories collection

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db("cadence-watches").collection("categories");
+        const usersCollection = client.db("cadence-watches").collection("users");
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+    }
+    finally { }
+
+}
+
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('Server is Running!!');
