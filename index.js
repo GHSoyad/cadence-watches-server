53aeb62a0e1eae65fd653f33bee37517173b8c10
GHSoyad <ghsoyad@gmail.com>
2022-11-25 15:45:25 +0600

    Added category specific product fetching with filtered status

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +21,13 @@ async function run() {
         app.get('/categories', async (req, res) => {
             const query = {};
             const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category: id, status: 'available' };
+            const result = await productsCollection.find(query).toArray();
             res.send(result);
         })
 
