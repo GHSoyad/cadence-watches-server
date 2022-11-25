6c06f967ca1cb678c2858f2edda8276dccace0cc
GHSoyad <ghsoyad@gmail.com>
2022-11-25 17:06:43 +0600

    Added jwt and jwt validation middleware

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized User' })
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+app.get('/jwt', (req, res) => {
+    const email = req.query.email;
+    const token = jwt.sign({ email }, process.env.JWT_ACCESS_TOKEN, { expiresIn: '24h' });
+    res.send({ token });
+})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,7 +46,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/category/:id', async (req, res) => {
+        app.get('/category/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { category: id, status: 'available' };
             const result = await productsCollection.find(query).toArray();
